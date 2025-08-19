4b2b2ea3c79b40ba369a1039a171f3c3dde0dc63
Jonathan <jonathancabatoupwork@gmail.com>
2025-08-19 17:15:50 +0800

    Increased version to 1.0.1

--- wf-settings.php
+++ wf-settings.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Wirefront Settings Framework
  * Description: A lightweight developer-first WordPress settings framework that lets you define plugin options using a structured array. Includes all essential input types—textbox, checkbox, radio, select, file upload with WordPress media library integration, sliders, and more. Designed for fast prototyping and reuse across projects. Easily retrieve saved values. Perfect for teams building modular, consistent, and scalable plugins.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Jonathan Cabato
  * Author URI: https://wirefront.net
  * License: GPL v2 or later
